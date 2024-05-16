ef1d59d77dd68f1f952435684840fff956421790
justinkropp <justinkropp@gmail.com>
2024-05-16 19:59:55 -0400

    no more jekyll

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './_drafts/**/*.html',
-      './_includes/**/*.html',
-      './_layouts/**/*.html',
-      './_posts/*.md',
-      './*.md',
-      './*.html',
-    ],
-    darkMode: 'class',
-    theme: {
-      fontFamily: {
-        mono: ['GeistMono', 'monospace'],
-        serif: ['Tiempos-Text'],
-        headline: ['Tiempos-Headline'],
-        logotype: ['Soehne-Breit'],
-        headlinelight: ['Tiempos-Headline-Light'],
-        sans: ['Soehne-Buch', 'HelveticaNeue', 'Helvetica Neue', 'Helvetica'],
-        sanslight: ['Soehne-Leicht'],
-      },
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      serif: ['Tiempos-Text'],
+      headline: ['Tiempos-Headline'],
+      headlinelight: ['Tiempos-Headline-Light'],
 
-      extend: {
-        fontFamily: {
-        },
-
-      },
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography'),
-    ]
-  }
-  
+    extend: {},
+  },
+  plugins: [],
+}
+
