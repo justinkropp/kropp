c39dac84cecaff6f2b9b8505065857af60605ba5
justinkropp <justinkropp@gmail.com>
2025-03-21 15:40:21 -0400

    adding bio

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        <p className="text-sm font-[family-name:var(--font-geist-mono)]">
-        Justin Kropp</p>
-        <p className="text-sm font-[family-name:var(--font-geist-mono)] text-[#666]">
-        Graphic Designer turned product designer, currently at Vercel.
-        </p>
+      <main className="w-lg">
+        <p className="font-[family-name:var(--font-geist-sans)] text-[#666] mb-4">
+        <strong className="font-medium text-[#222]">Justin Kropp</strong> is a graphic designer and writer based out of Chattanooga, Tennessee. He studied design at <a href="https://www.mica.edu/graduate-programs/graphic-design-mfa/" className="text-[#EB5E4A]">Maryland Institute College of Art</a>, where he received his MFA in Graphic Design in 2010.</p>
+        <p className="font-[family-name:var(--font-geist-sans)] text-[#666]">He is currently a product designer at <a href="https://vercel.com" className="text-[#EB5E4A]">Vercel</a> — previously at <a href="https://www.moderntreasury.com/" className="text-[#EB5E4A]">Modern Treasury</a>, <a href="https://www.mozilla.org" className="text-[#EB5E4A]">Mozilla</a>, and <a href="https://automattic.com/" className="text-[#EB5E4A]">Automattic</a> &mdash; designing features and novel experiences with multidiscplinary teams.</p>
       </main>
     </div>
   );
