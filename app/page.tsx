88621f315349512f89af83deb592c3050887c7bf
justinkropp <justinkropp@gmail.com>
2025-03-21 11:45:32 -0400

    updating git repo

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className="">
+        <p className="text-sm font-[family-name:var(--font-geist-mono)]">
+        Justin Kropp</p>
+        <p className="text-sm font-[family-name:var(--font-geist-mono)] text-[#666]">
+        Graphic Designer turned product designer, currently at Vercel.
+        </p>
+
+      </main>
+    </div>
+  );
+}
