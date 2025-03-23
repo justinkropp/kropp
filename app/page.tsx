49975d083bf15d8bd1b4550e7a52f2111a0aef11
justinkropp <justinkropp@gmail.com>
2025-03-23 10:29:39 -0400

    updating bg color

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="max-w-lg">
-        <p className="font-[family-name:var(--font-geist-sans)] text-[#888] mb-4">
-        <strong className="font-medium text-[#eee]">Justin Kropp</strong> is a graphic designer and writer based out of the Southeastern United States in the foothills of the Smoky Mountains. He studied design at <a href="https://www.mica.edu/graduate-programs/graphic-design-mfa/" className="text-[#eee]">Maryland Institute College of Art</a>, where he received his MFA in Graphic Design in 2010.</p>
-        <p className="font-[family-name:var(--font-geist-sans)] text-[#888]">He is currently a product designer at <a href="https://vercel.com" className="text-[#eee]">Vercel</a> — previously at <a href="https://www.moderntreasury.com/" className="text-[#eee]">Modern Treasury</a>, <a href="https://www.mozilla.org" className="text-[#eee]">Mozilla</a>, and <a href="https://automattic.com/" className="text-[#eee]">Automattic</a> &mdash; designing and building tools for the frontend cloud.</p>
+        <p className="font-[family-name:var(--font-geist-sans)] text-blue-200 mb-4">
+        <strong className="font-medium text-[#fff]">Justin Kropp</strong> is a graphic designer and writer based out of the Southeastern United States in the foothills of the Smoky Mountains. He studied design at <a href="https://www.mica.edu/graduate-programs/graphic-design-mfa/" className="text-[#fff]">Maryland Institute College of Art</a>, where he received his MFA in Graphic Design in 2010.</p>
+        <p className="font-[family-name:var(--font-geist-sans)] text-blue-200">He is currently a product designer at <a href="https://vercel.com" className="text-[#eee]">Vercel</a> — previously at <a href="https://www.moderntreasury.com/" className="text-[#fff]">Modern Treasury</a>, <a href="https://www.mozilla.org" className="text-[#fff]">Mozilla</a>, and <a href="https://automattic.com/" className="text-[#fff]">Automattic</a> &mdash; designing and building tools for the frontend cloud.</p>
       </main>
     </div>
   );
